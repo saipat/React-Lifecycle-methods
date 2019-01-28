53cebf82d799b639435e9b6276b1ee0bda268550
Sai Lakshmi Pattabiraman <sailakshmi.pattabiraman@gmail.com>
2019-01-28 11:17:31 -0800

    Understand the bind and arraow functions

--- src/App.js
+++ src/App.js
@@ -4,13 +4,37 @@ class App extends Component {
 
   static displayName = "Namin the app"
 
-  state = {
-    outInitialState: "golden"
+  constructor(props) {
+    super(props);
+    this.function1 = this.function1.bind(this);
+  }
+
+  function1() {
+    console.log("function 1");
+    console.log(this.props);
+  }
+
+  useArrows = () => {
+    console.log("use arraow without binding");
+    console.log(this.props);
   }
 
   render() {
+    console.log(this.state);
     return (
-      <h1>Hello</h1>
+      <div>
+        <button
+          onClick={this.function1}
+        >
+          test function 1
+        </button>
+        < button
+        onClick = {
+            this.useArrows
+          } >
+          testuseArrows 
+          </button>
+      </div>
     );
   }
 }
